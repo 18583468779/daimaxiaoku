d6030df4c2ac9db29c2902b973355cca9899ea79
xiewen <1123654054@qq.com>
2024-11-24 11:37:05 +0800

    vite配置开发环境，以及http请求代理

--- daimaxiaoku/README.md
+++ daimaxiaoku/README.md
@@ -8,3 +8,5 @@
 2. .env.development 表示开发环境下的文件
 3. .env 表示备选环境文件
    注意：vite 只处理以 VITE 开头的环境变量
+
+## 安装 dotenv
--- daimaxiaoku/vite.config.ts
+++ daimaxiaoku/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, CommonServerOptions } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig((mode) => {
+  const envFileName: string = ".env";
+  const curEnvFileName = `${envFileName}.${mode.mode}`; // 配置环境变量的文件
+  console.log("curEnvFileName", curEnvFileName);
+  let server: CommonServerOptions = {}; // 配置服务开发
+  if (mode.mode === "development") {
+    server = {
+      host: "192.168.153.1",
+      port: 5005,
+      proxy: {
+        // 代理
+        "/dmxk": {
+          target: "http://192.168.153.1:5003",
+        },
+      },
+    };
+    console.log("开发者模式环境");
+  } else if (mode.mode === "production") {
+    console.log("生产者模式环境");
+  }
+  return {
+    // base: "dxmk", // 项目基础根路径
+    plugins: [vue()],
+    server,
+  };
+});
